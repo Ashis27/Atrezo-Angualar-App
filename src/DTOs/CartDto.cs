fca6b59eb57c504993ad9738dfb2f8012b8d9510
Ashis Mahapatra <Ashis.Mahapatra@cai.io>
2020-07-29 19:51:00 +0530

    View model has been added for cart and product item

---
+++ src/DTOs/CartDto.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EShopOnPromotionEngineeRule.API.Dtos
+{
+    public class CartDto
+    {
+        public string CartId { get; set; }
+        public List<CartItemDto> CartItems { get; set; }
+        public decimal TotalPrice
+        {
+            get
+            {
+                return CartItems.Sum(p => p.OfferPrice);
+            }
+        }
+    }
+
+    public class CartItemDto
+    {
+        [Required]
+        public string SKU { get; set; }
+        [Required]
+        public decimal UnitPrice { get; set; }
+        [Required]
+        public int Quantity { get; set; }
+        public decimal OfferPrice { get; set; }
+        public decimal ActualPrice
+        {
+            get
+            {
+                return UnitPrice * Quantity;
+            }
+        }
+        public bool PromoApplied { get; set; }
+        public string PromoId { get; set; }
+    }
+}
